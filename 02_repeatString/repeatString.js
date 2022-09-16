6f46b0bcb157532c5b832313e0ad76e33e2be397
NateVoyages <allanromo20@gmail.com>
2022-09-16 09:33:15 -0700

    Finish exercises 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,23 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  let stringHolder = string;
+
+  for(let i = 0; i < num; i++)
+  {
+    if(i === 0){
+    string = string;
+    }
+    else{
+      string = string + stringHolder;
+    } 
+  }
+  if (num === 0){
+    string = '';
+  }
+  else if(num < 0){
+    string = 'ERROR';
+  }
+
+  return string;
 
 };
 
