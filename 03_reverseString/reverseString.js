5fd42e9d58cf91452b25cb009cddbe1c493d9d7b
NateVoyages <allanromo20@gmail.com>
2022-09-16 12:11:27 -0700

    Finish exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  let charArray =  string.split("");
+  let arrayHolder = "nothing";
 
+  for (let i = 0; i < string.length/2; i++){
+    arrayHolder = charArray[i];
+    charArray[i] = charArray[string.length - 1 - i];
+    charArray[string.length - 1 - i] = arrayHolder;
+  }
+  string = charArray.join('');
+  return string;
 };
 
 // Do not edit below this line
