cfc98bb92887f2c32c8a9c584ba48dd82439358a
NateVoyages <allanromo20@gmail.com>
2022-09-17 10:12:34 -0700

    Pass first test + fail second

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removedMore) {
+  let arrayResult = [];
 
+  for(let i = 0; i < array.length; i++)
+  {
+    for(let j = 0; j < removedMore.length; j++){
+      if (removedMore[j] === array [i]){
+        array[i] = undefined;
+      }
+    if (array[i] !== undefined){
+        arrayResult.push(array[i]);
+      }
+    }
+  }
+  
+  return arrayResult;
 };
 
 // Do not edit below this line
