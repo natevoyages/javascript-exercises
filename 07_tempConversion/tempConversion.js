acc8d4dc8debdd4163c743e82b857275e967a012
NateVoyages <allanromo20@gmail.com>
2022-09-17 21:05:37 -0700

    Finish 06 and 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let temp = (f - 32) * (5/9);
+  if (f !== 32){
+  temp = +(temp.toFixed(1));
+  }
+  return temp;
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let temp = 32 + ( c  * (9/5) );
+  if (c % 5 !== 0){
+    temp = +(temp.toFixed(1));
+    }
+  return temp;
 };
 
 // Do not edit below this line
