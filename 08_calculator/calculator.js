b5972e54ea2a8fee71b4622a1a8247c742c8c4fb
NateVoyages <allanromo20@gmail.com>
2022-09-22 20:33:59 -0700

    Finish exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,51 @@
-const add = function() {
+const add = function(numOne, numTwo) {
+  
+	return numOne + numTwo;
+};
+
+const subtract = function(numOne, numTwo) {
+  return numOne - numTwo;
 	
 };
 
-const subtract = function() {
+const sum = function(numArray) {
+ let sum = 0;
+ if (numArray === null){
+ }	
+ else{
+
+ numArray.forEach(num => {sum = sum + num; 
+  
+     });
+   }
+   return sum;
+};
+
+const multiply = function(numArray) {
+  let product = 1;
+  numArray.forEach(num => (product = product * num)); 
+
+  return product;
+};
+
+const power = function(base, power) {
+  return base ** power;
 	
 };
 
-const sum = function() {
-	
-};
+const factorial = function(num) {
+  let result = 1;
+  if (num < 2){
 
-const multiply = function() {
+  }
+  else{
+    for(let i = 2; i <= num; i++ )
+    {
+      result = result * i;
+    }
+  }
 
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+  return result;
 	
 };
 
