445a93cd0af36b57a7b23062d51aa497b14f80dd
NateVoyages <allanromo20@gmail.com>
2022-09-23 15:22:41 -0700

    Finish exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+ string = string.toLowerCase();
+ let newString = string.split('');
+ string = newString;
+ newString.forEach(char => {
+    if(/[^a-z]/.test(char)){
+        let index = newString.indexOf(char);
+        newString.splice(index, 1);
+        string = newString;
+     }
+     if(/[\s]/.test(char)){
+        let index = newString.indexOf(char);
+        newString.splice(index, 1);
+        string = newString;
+     }
+    
+ });
+  string = string.join('');
+  newString.reverse();
+  newString = newString.join('');
+  return newString === string;
 };
 
 // Do not edit below this line
