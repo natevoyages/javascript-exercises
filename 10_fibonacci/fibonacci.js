837e01abf94f7d2eadac233b0b244d89c5eeaafb
NateVoyages <allanromo20@gmail.com>
2022-09-23 16:00:45 -0700

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let result = "OOPS";
+    prevA = 1;
+    prevB = 1;
+    if (number > 0){
+        if (number == 1 || number == 2){
+        result = 1; 
+        }
+        else{
+          for(let i = 2; i < number; i++){
+            result = prevA + prevB;
+            prevA = prevB;
+            prevB = result;
+          }
+        }
+    }
+    
+    return result;
 };
 
 // Do not edit below this line
