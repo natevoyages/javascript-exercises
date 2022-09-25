71c39df93e3e5bf964986fb072c1213738174013
NateVoyages <allanromo20@gmail.com>
2022-09-24 23:10:01 -0700

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,31 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+
+
+const result =  people.reduce((oldest, person) => {
+
+  if(oldest === 0){
+    oldest = person;
+  }
+  else{
+    if(!person.yearOfDeath){
+      let d = new Date();
+      person.yearOfDeath = d.getFullYear();
+    }
+    if(!oldest.yearOfDeath){
+      let d = new Date();
+      oldest.yearOfDeath = d.getFullYear();
+    }
+    let oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+    let personAge = person.yearOfDeath - person.yearOfBirth;
+    if(oldestAge < personAge){
+      oldest = person;
+    }
+  }
+  return oldest;
+
+}, 0);
+return result;
 
 };
 
